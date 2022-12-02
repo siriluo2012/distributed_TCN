f5a222f55c06f9357d28ecd75b39b39da034a36b
siriluo2012 <56318001+siriluo2012@users.noreply.github.com>
2022-12-02 11:15:43 -0600

    Create submit_1_gpu.sh

---
+++ submit_1_gpu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name="tensorflow_mirroredstrategy"
+#SBATCH --output="tensorflow_mirroredstrategy_%j.out"
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cores-per-socket=10
+#SBATCH --threads-per-core=4
+#SBATCH --sockets-per-node=1
+#SBATCH --mem-per-cpu=1200
+#SBATCH --export=ALL
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=2:00:00
+
+module load conda_base
+
+conda activate shirui_env
+
+python ./TCN_distributed.py --epochs 20 --batch_size 32 --dataPath '/home/shirui/student_consulting/seid_distributed/'
+
